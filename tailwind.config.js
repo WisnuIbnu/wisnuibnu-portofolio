360744af6a9dfe2eb8135fc900342949b4b76d08
Wisnu Ibnu Muttaqiem <wisnuiben21@gmail.com>
2025-01-04 19:04:58 +0700

    portofolio wisnu

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '40px'
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+        secondary: '#64748b'
+      }
+    }
+  },
+  plugins: [],
+}
+
